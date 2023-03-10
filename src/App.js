17e139c00b57649a9cfeaf946efff05952f93800
zaiky09 <zamilmozamil@outlook.com>
2023-03-10 10:25:31 +0300

    Added Bootstrap to index.html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import NavBar from './NavBar';
+import NavBar from './components/navbar/NavBar';
 //import SignUp from './SignUp';
 //import Login from './Login';
-import HomePage from './HomePage';
-import FoodList from './FoodList';
+import HomePage from './components/herosection/HomePage';
+import FoodList from './components/foodlist/FoodList';
 //import Search from './Search';
 //import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
